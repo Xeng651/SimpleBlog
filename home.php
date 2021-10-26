e32fcd41d2990672f6bcda6ba97de3016d888803
Xeng651 <84945092+Xeng651@users.noreply.github.com>
2021-10-26 11:56:09 -0500

    Update home.php

--- home.php
+++ home.php
@@ -1,24 +1,67 @@
-
 <?php require_once("includes/head_section.php") ?>
-	<title> Simple Blog </title>
+<title> Simple Blog </title>
+<style>
+	.addlink {
+		position: relative;
+		top: 10px;
+		right: 10px;
+		float: right;
+	}
+
+	.addalink:link:hover {
+		background-color: darkgreen;
+	}
+
+	.addalink:link {
+		color: whitesmoke;
+		display: block;
+		width: 100px;
+		height: 40px;
+		background-color: forestgreen;
+		box-sizing: border-box;
+		border-radius: 15px;
+		text-align: center;
+		font-size: 15px;
+	}
+
+	.addalink:visited {
+		color: whitesmoke;
+		display: block;
+		width: 200px;
+		height: 50px;
+		background-color: forestgreen;
+		box-sizing: border-box;
+		text-align: center;
+	}
+
+	.addalink {
+		text-decoration: none;
+		padding-top: 10px;
+	}
+</style>
 </head>
+
 <body>
 	<div class="container">
 		<!-- navbar -->
 		<?php include("includes/navbar.php") ?>
-		<!-- /navbar -->
+
+		<div class="addlink">
+			<a href="newpost.php" class="addalink"> Add Post + </a>
+		</div>
 
 		<!-- Page content -->
 		<div class="content">
-			<h2 class="content-title"> </h2>
-			<hr>
-			<!-- more content still to come here ... -->
-		</div>
-		<!-- /Page content -->
 
-<!-- footer -->
-<?php include("includes/footer.php") ?>
-<!-- /footer -->
+
+		</div>
+
+		<!-- footer -->
+		<?php include("includes/footer.php") ?>
+
+</body>
+
+</html>
 
 <!-- 
 
