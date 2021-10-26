b6aa9166c0603391aeb65675bba0f51bafac53a3
Xeng651 <84945092+Xeng651@users.noreply.github.com>
2021-10-26 11:55:53 -0500

    Update login.php

--- login.php
+++ login.php
@@ -1,10 +1,27 @@
 <!DOCTYPE html>
 <html>
+<title> Login Blog </title>
 
 <head>
     <style>
+        * {
+            margin: 0px;
+            padding: 0px;
+        }
+
+        html {
+            height: 100%;
+        }
+
         body {
+            margin: 0;
+            padding: 0;
             background-color: beige;
+            position: relative;
+            font-family: Arial;
+            min-height: 100%;
+            overflow: hidden;
+            width: 100%;
         }
 
         .footer {
@@ -15,7 +32,7 @@
             background-color: forestgreen;
             text-align: center;
             width: 100%;
-            padding: 5px;
+            padding: 15px;
         }
 
         table,
@@ -24,34 +41,54 @@
         }
 
         .table_title {
+            height: 100px;
             text-align: center;
             background-color: forestgreen;
             color: whitesmoke;
         }
 
-        .lables {
+        .labels {
+            height: 50px;
             background-color: forestgreen;
             color: whitesmoke;
         }
 
         .sign_up_form {
             position: absolute;
-            top: 30%;
-            left: 45%;
+            top: 20%;
+            left: 35%;
+        }
+
+        input[type="text"] {
+            width: 300px;
         }
 
         input[type="submit"] {
-            color: white;
+            color: whitesmoke;
         }
 
         .submit_1 {
+            width: 100px;
+            height: 50px;
             position: relative;
-            right: 5px;
+            right: 20px;
+            top: 20px;
             background-color: forestgreen;
+            cursor: pointer;
         }
 
         .submit_2 {
+            position: relative;
+            width: 100px;
+            height: 50px;
+            top: 20px;
             background-color: forestgreen;
+            cursor: pointer;
+        }
+
+        .submit_2:hover,
+        .submit_1:hover {
+            background-color: darkgreen;
         }
 
         .logo img {
@@ -62,6 +99,55 @@
             width: 120px;
             float: left;
             background-color: beige;
+        }
+
+        table {
+            width: 500px;
+        }
+
+        .submitrow {
+            height: 100px;
+        }
+
+        .heading {
+            position: relative;
+            top: 100px;
+            left: 620px;
+            color: black;
+        }
+
+        .goto_create {
+            position: relative;
+            top: 10px;
+            right: 10px;
+            float: right;
+        }
+
+        .signuplink:link {
+            color: whitesmoke;
+            display: block;
+            width: 150px;
+            height: 50px;
+            background-color: forestgreen;
+            box-sizing: border-box;
+            border-radius: 15px;
+            text-align: center;
+            font-size: 23px;
+        }
+
+        .signuplink:visited {
+            color: whitesmoke;
+            display: block;
+            width: 200px;
+            height: 50px;
+            background-color: forestgreen;
+            box-sizing: border-box;
+            text-align: center;
+        }
+
+        .signuplink {
+            text-decoration: none;
+            padding-top: 10px;
         }
     </style>
 </head>
@@ -74,6 +160,14 @@
         <img src="images/logo.png" alt="logo">
     </div>
 
+    <div class="goto_create">
+        <a href="register.php" class="signuplink"> Sign Up </a>
+    </div>
+
+    <div class="heading">
+        <h1> Log in to your account </h1>
+    </div>
+
     <div class="sign_up_form">
         <table>
             <form action="" method="POST">
@@ -81,21 +175,19 @@
                     <td colspan="2" class="table_title">Login Information</td>
                 </tr>
                 <tr>
-                    <td class="lables">Email:</td>
-                    <td class="input"><input type="text" name="email" required></td>
+                    <td class="labels">Email:</td>
+                    <td class="input"><input type="text" name="email"></td>
                 </tr>
                 <tr>
-                    <td class="lables">Password</td>
-                    <td class="input"><input type="password" name="login_pass" required></td>
+                    <td class="labels">Password</td>
+                    <td class="input"><input type="password" name="login_pass"></td>
                 </tr>
-                <tr>
+                <tr class="submitrow">
                     <td><input type="submit" name="action" value="Cancel" class="submit_2"></td>
                     <td><input type="submit" name="action" value="Login" class="submit_1"></td>
                 </tr>
             </form>
-
         </table>
-
     </div>
     <?php include("includes/footer.php") ?>
 
