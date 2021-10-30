ef638651c65dad7d948775c79a9a7646b46b6626
Xeng651 <84945092+Xeng651@users.noreply.github.com>
2021-10-30 13:48:53 -0500

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,8 +1,4 @@
-
-		<div class="footer">
-            <p> Author: Xeng Xiong, Seng Khang, Xeng Lee </p>
-            <p>&copy; <?php echo date('Y'); ?></p>
-		</div>
-		
-</body>
-</html>
+<footer id="footer">
+      <p> Author: Xeng Xiong, Seng Khang, Xeng Lee </p>
+      <p>&copy; <?php echo date('Y'); ?></p>
+</footer>
